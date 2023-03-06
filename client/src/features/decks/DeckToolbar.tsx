d710e89e91462038aaf08734b17cd376066b6213
John He <47208192+John-I-Am@users.noreply.github.com>
2023-03-07 00:44:42 +1300

    Add card search functionality to decktoolbar

---
+++ client/src/features/decks/DeckToolbar.tsx
@@ -0,0 +1,101 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import { ReactElement } from "react";
+import {
+  Modal, ActionIcon, Text, Group, Input, Stack,
+} from "@mantine/core";
+import { openConfirmModal } from "@mantine/modals";
+import { useDisclosure } from "@mantine/hooks";
+import { IconPlus, IconTrash, IconEdit } from "@tabler/icons-react";
+import { useForm } from "react-hook-form";
+import { Deck } from "types";
+import { useAppDispatch } from "../../hooks/hooks";
+import CardEditor from "./CardEditor/CardEditor";
+import { useDeleteDeckMutation, useUpdateDeckMutation } from "../api/apiSlice";
+import { setActive } from "./decksSlice";
+import SearchBar from "../../components/SearchBar";
+
+interface FormValues {
+  title: string;
+}
+
+const DeckToolbar = ({ deck, searchCallback }: {deck: Deck, searchCallback: Function})
+  : ReactElement => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const [deleteDeck] = useDeleteDeckMutation();
+  const [updateDeck] = useUpdateDeckMutation();
+  const dispatch = useAppDispatch();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm<FormValues>();
+
+  const handleChangeTitle = async (data: {title: string}): Promise<void> => {
+    await updateDeck({ activeDeckId: deck.id, title: data });
+    setValue("title", data.title);
+  };
+
+  const handleDeleteDeck = (): void => {
+    openConfirmModal({
+      title: "Confirm Action",
+      children: (
+        <Text transform="uppercase" weight={700} c="red" size="sm">
+          Deleted decks are unrecoverable.
+        </Text>
+      ),
+      labels: { confirm: "Confirm", cancel: "Cancel" },
+      onConfirm: () => {
+        deleteDeck(deck?.id);
+        dispatch(setActive(null));
+      },
+    });
+  };
+
+  return (
+    <>
+      <Modal opened={opened} onClose={close} size="xl" withCloseButton={false}>
+        <CardEditor card={undefined} />
+      </Modal>
+      <Group px="md" bg="white" position="apart">
+        <Stack spacing="xs">
+          <form onSubmit={handleSubmit(handleChangeTitle)}>
+            <Input
+              {...register("title", {
+                required: true,
+                pattern: {
+                  value: /^[a-zA-Z0-9_ ]{1,20}$/i,
+                  message: "length exceeded / invalid characters",
+                },
+              })}
+              disabled={!deck?.id}
+              placeholder={deck?.title}
+              icon={<IconEdit />}
+            />
+          </form>
+          <Text c="red" fz="sm" role="alert">
+            {errors.title && errors.title.message}
+          </Text>
+          <Text c="red" fz="sm" role="alert">
+            {errors.title && errors.title.type}
+          </Text>
+        </Stack>
+
+        <SearchBar callback={searchCallback} />
+        <Group>
+          <Group>
+            <ActionIcon size="xl" disabled={!deck?.id} onClick={() => open()}><IconPlus /></ActionIcon>
+            <Text fz="sm">New Card</Text>
+          </Group>
+          <Group>
+            <ActionIcon size="xl" disabled={!deck?.id} onClick={handleDeleteDeck}><IconTrash /></ActionIcon>
+            <Text fz="sm">Delete Deck</Text>
+          </Group>
+        </Group>
+      </Group>
+    </>
+  );
+};
+
+export default DeckToolbar;
--- client/src/pages/DeckEditorPage/DeckEditorPage.tsx
+++ client/src/pages/DeckEditorPage/DeckEditorPage.tsx
@@ -1,16 +1,25 @@
-import { ReactElement } from "react";
+import { ReactElement, useState } from "react";
+import { Card } from "types";
 import CardList from "../../features/decks/CardList/CardList";
-import DeckEditor from "../../features/decks/DeckEditor/DeckEditor";
+import DeckToolbar from "../../features/decks/DeckToolbar";
 import { useActiveDeck } from "../../hooks/hooks";
 import { Container } from "./styles";
 
 const DeckEditorPage = (): ReactElement => {
   const activeDeck = useActiveDeck();
+  const [filter, setFilter] = useState<string>("");
+
+  // cardsToShows filters front vaule based on filter variable
+  const cardsToShow: Card[] | any[] = filter === ""
+    ? activeDeck.cards ?? []
+    : activeDeck.cards?.filter(
+      (cards) => cards.front.toLowerCase().includes(filter.toLowerCase()),
+    ) ?? [];
 
   return (
     <Container>
-      <DeckEditor />
-      <CardList cards={activeDeck?.cards ?? []} />
+      <DeckToolbar deck={activeDeck} searchCallback={setFilter} />
+      <CardList cards={cardsToShow} />
     </Container>
   );
 };
