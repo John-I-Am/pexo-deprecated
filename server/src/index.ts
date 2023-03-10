ef7866675ba6bd60d592644f2007066c82803328
John He <47208192+John-I-Am@users.noreply.github.com>
2023-03-10 18:04:28 +1300

    fix(build): fix migration/typescript error
    - Dispite changing nothing, server suddently complains
    about "unable to use import statement outside of module".
    Asks to require ts-node before migration.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-unused-expressions */
 import http from "http";
 import config from "./utils/config";
 import app from "./app";
 import logger from "./utils/logger";
+
+require("ts-node/register"); // ??????? <=== app suddenly needs this line to function dispite changing nothing
 
 const server = http.createServer(app);
 
