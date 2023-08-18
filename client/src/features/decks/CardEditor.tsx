c10402f0536a9b80ca41c983d870c552c9b2dac1
John He <47208192+John-I-Am@users.noreply.github.com>
2023-08-18 16:41:24 +1200

    refactor(cardEditor): refactor for better extensibility
    - modify UI and code structure to make it easier to add new card types

--- client/src/features/decks/CardEditor.tsx
+++ client/src/features/decks/CardEditor.tsx
@@ -3,11 +3,14 @@ import { ReactElement, useState } from "react";
 import { useForm } from "react-hook-form";
 
 import {
-  Textarea, TextInput, Button, Radio, Badge, Group, Stack,
+  Textarea, TextInput, Button, Badge, Group, Stack, ActionIcon, Text,
 } from "@mantine/core";
 import { showNotification } from "@mantine/notifications";
 import { Card, CardType, NewCard } from "types";
 
+import {
+  Icon360, IconPlus, IconUnderline,
+} from "@tabler/icons-react";
 import dictionaryService from "../../services/dictionary";
 import { useAppSelector } from "../../hooks/hooks";
 import { useCreateCardMutation, useUpdateCardMutation } from "../api/apiSlice";
@@ -36,6 +39,8 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
   const activeDeckId = useAppSelector((state: any) => state.decks.activeDeckId);
   const [tags, setTags] = useState(card ? card.tags : []);
 
+  const [mode, setMode] = useState<CardType>(card?.type || "classic");
+
   const [createCard, { isLoading: isLoadingCreate }] = useCreateCardMutation();
   const [updateCard, { isLoading: isLoadingUpdate }] = useUpdateCardMutation();
 
@@ -56,6 +61,7 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
   const {
     register: registerTag,
     handleSubmit: handleSubmitTag,
+    setValue: setValueTag,
     setError: setErrorTag,
     formState: { errors: errorsTag },
   } = useForm<FormValueTag>();
@@ -78,6 +84,7 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
   const handleCreateCard = async (data: FormValueCard): Promise<void> => {
     const newCard: NewCard = {
       ...data,
+      type: mode,
       examples: data?.examples ? data.examples.split("") : [], // temporary solution for examples needing to be in array. need to refactor backend
       tags,
       deckId: activeDeckId,
@@ -87,6 +94,7 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
       const updatedCard = {
         ...card,
         ...data,
+        type: mode,
         level: undefined,
         examples: data?.examples ? data.examples.split("") : [], // temporary solution for examples needing to be in array. need to refactor backend
         tags,
@@ -113,6 +121,7 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
   const handleAddTag = ({ tag }: {tag: string}) => {
     if (!(tags?.includes(tag))) {
       setTags(tags?.concat(tag));
+      setValueTag("tag", "");
     } else {
       setErrorTag("tag", {
         type: "manual",
@@ -125,71 +134,80 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
     <Stack
       id="cardEditor"
     >
-      <form onSubmit={handleSubmit(handleSearchWord)}>
-        <TextInput
-          error={errors.word?.message}
-          label="Autogenerate Vocabulary Card"
-          {...register("word", {
-            required: "required",
-          })}
-        />
+      <Group noWrap spacing="lg">
+        <Stack spacing="sm" align="center">
+          <ActionIcon size="5rem" color="blue" variant={mode === "classic" ? "filled" : "light"} onClick={() => setMode("classic")}>
+            <Icon360 size="2rem" />
+          </ActionIcon>
+          <Text fz="sm">Classic</Text>
+          <Text fz="xs" c="dimmed" lh="0">flip to reveal</Text>
+        </Stack>
 
-        <Button my="md" compact type="submit">
-          Generate
-        </Button>
-      </form>
+        <Stack spacing="sm" align="center">
+          <ActionIcon size="5rem" color="blue" variant={mode === "cloze" ? "filled" : "light"} onClick={() => setMode("cloze")}>
+            <IconUnderline size="2rem" />
+          </ActionIcon>
+          <Text fz="sm">Cloze</Text>
+          <Text fz="xs" c="dimmed" lh="0">fill in the blanks</Text>
+        </Stack>
+      </Group>
 
       <form onSubmit={handleSubmitTag(handleAddTag)}>
-        <TextInput
-          {...registerTag("tag", {
-            required: "required",
-            maxLength: {
-              value: 15,
-              message: "Maximum of 15 characters",
-            },
-          })}
-          placeholder="tag"
-          label="Tags"
-          error={errorsTag.tag?.message}
-        />
-        <Button mt="xs" compact type="submit">
-          Add
-        </Button>
+        <Group spacing="sm" p="sm" sx={(theme) => ({ border: ".5px solid grey", borderRadius: theme.radius.sm })}>
+          {tags?.map((tag: string) => <Badge key={tag}>{tag}</Badge>)}
+          <Group noWrap>
+            <TextInput
+              radius="xl"
+              styles={(theme) => ({
+                input: {
+                  border: "none",
+                  background: theme.colorScheme === "light" ? theme.colors.gray[0] : theme.colors.dark[5],
+                },
+              })}
+              {...registerTag("tag", {
+                required: true,
+                maxLength: {
+                  value: 15,
+                  message: "Max 15 characters",
+                },
+                pattern: /^(?!\s*$).+/,
+              })}
+              placeholder="add tag"
+              error={errorsTag.tag?.message}
+            />
+
+            <ActionIcon color="blue" type="submit" variant="filled">
+              <IconPlus />
+            </ActionIcon>
+          </Group>
+        </Group>
       </form>
 
-      <Group spacing="xs">
-        {tags?.map((tag: string) => <Badge key={tag}>{tag}</Badge>)}
+      <Group>
+        <form onSubmit={handleSubmit(handleSearchWord)}>
+          <TextInput
+            // rightSection={<Loader size="xs" />}
+            error={errors.word?.message}
+            label="Autofill"
+            description="Generates vocabulary card"
+            {...register("word", {
+              required: true,
+            })}
+          />
+
+          <Button my="md" compact type="submit">
+            Generate
+          </Button>
+        </form>
       </Group>
 
       <form onSubmit={handleSubmitCard(handleCreateCard)}>
-        <Radio.Group
-          mb="md"
-          name="cardMode"
-          label="Select mode"
-          defaultValue={card ? card.type : "classic"}
-        >
-          <Group mt="xs">
-            <Radio
-              checked
-              value="classic"
-              label="Classic"
-              description="Both sides will be visible"
-              {...registerCard("type")}
-            />
-            <Radio
-              value="cloze"
-              label="Cloze"
-              description="Fill in the blank"
-              id="input_cloze"
-              {...registerCard("type")}
-            />
-          </Group>
-        </Radio.Group>
-
+        {mode === "classic" && (
         <Group grow>
           <Textarea
             id="input_front"
-            placeholder="This will be your answer key in cloze mode"
+            autosize
+            placeholder="Front of card"
             label="Front"
             error={errorsCard.front?.message}
             defaultValue={card?.front}
@@ -204,6 +222,8 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
 
           <Textarea
             id="input_back"
+            autosize
+            placeholder="Back of card"
             {...registerCard("back", {
               required: "required",
               maxLength: {
@@ -216,10 +236,49 @@ const CardEditor = ({ card }: CardEditorProp): ReactElement => {
             error={errorsCard.back?.message}
           />
         </Group>
+        )}
+
+        {/* / TODO: implement custom editor for cloze type. currently same as classic  */}
+        {mode === "cloze" && (
+        <Group grow>
+          <Textarea
+            id="input_front"
+            autosize
+            placeholder="Front of card"
+            label="Front"
+            error={errorsCard.front?.message}
+            defaultValue={card?.front}
+            {...registerCard("front", {
+              required: "required",
+              maxLength: {
+                value: 254,
+                message: "Maximum characters of 254.",
+              },
+            })}
+          />
+
+          <Textarea
+            id="input_back"
+            autosize
+            placeholder="Back of card"
+            {...registerCard("back", {
+              required: "required",
+              maxLength: {
+                value: 254,
+                message: "Maximum characters of 254.",
+              },
+            })}
+            defaultValue={card?.back}
+            label="Back"
+            error={errorsCard.back?.message}
+          />
+        </Group>
+        )}
 
         <Textarea
           id="input_notes"
-          placeholder="Your notes"
+          autosize
+          placeholder="Card notes..."
           label="Notes"
           error={errorsCard.examples?.message}
           defaultValue={card?.examples}
